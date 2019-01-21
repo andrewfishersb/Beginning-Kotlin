50f894c329c501640fb933a99c301d4823561da2
Andrew Fisher <andrewfishersb@gmail.com>
2019-01-20 18:08:51 -0800

    initial function

---
+++ src/AMS.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main(args : Array<String>){
+    feedTheFish()
+}
+
+fun feedTheFish(){
+    val day = "Tuesday"
+    val food = "pellets"
+    println("Today is $day and the fish will eat $food")
+}
