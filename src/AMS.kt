c2074658317676a960c32c9054c4872d6c47e42f
Andrew Fisher <andrewfishersb@gmail.com>
2019-01-20 18:16:00 -0800

    Create a method to randomize day of the week

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,11 +1,18 @@
 import java.util.*
+import kotlin.random.Random
 
 fun main(args : Array<String>){
     feedTheFish()
 }
 
 fun feedTheFish(){
-    val day = "Tuesday"
+    val day = randomDay()
     val food = "pellets"
     println("Today is $day and the fish will eat $food")
+}
+
+fun randomDay() : String{
+    val weekdays = listOf("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+
+    return weekdays.random()
 }
