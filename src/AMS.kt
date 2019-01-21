c04429034eaa1303f7ead77aa7c84e6846aaf20e
Andrew Fisher <andrewfishersb@gmail.com>
2019-01-20 23:03:53 -0800

    Add function to check if can add more fish

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,11 +1,23 @@
-import java.util.*
-import kotlin.random.Random
 
 fun main(args : Array<String>){
 
         feedTheFish()
 
+    println(canAddMoreFish(10.0, listOf(3,3,3)))
+    println(canAddMoreFish(8.0, listOf(2,2,2),hasDecorations = false))
+    println(canAddMoreFish(9.0, listOf(3,3,3),3))
+    println(canAddMoreFish(10.0, listOf(),7,true))
 
+}
+
+fun shouldChangeWater(day: String, temperature : Int = 22, dirty : Int = 20):Boolean{
+    return true
+
+}
+
+fun canAddMoreFish(tankSize : Double, currentFish : List<Int>, fishSize : Int = 2, hasDecorations : Boolean = true) : Boolean {
+
+    return (tankSize * if(hasDecorations) .8 else .1) >= (currentFish.sum() + fishSize)
 
 }
 
@@ -13,6 +25,9 @@ fun feedTheFish(){
     val day = randomDay()
     val food = fishFood(day)
     println("Today is $day and the fish will eat $food")
+    if(shouldChangeWater(day)){
+        println("Should change the water today")
+    }
 }
 
 fun randomDay() : String{
